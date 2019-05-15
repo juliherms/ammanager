bcdb52edbe6fc2d196bbce247f3134001e707762
Vasconcelos <j.a.vasconcelos@accenture.com>
2019-05-15 08:40:00 -0300

    adicao dos metodos basicos para o formulario de time

--- src/app/pages/times/time-form/time-form.component.ts
+++ src/app/pages/times/time-form/time-form.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { TimeService } from './../shared/time.service';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder,FormControl,FormGroup,Validators } from "@angular/forms";
+import { ActivatedRoute,Router } from "@angular/router";
+import { Time } from "../shared/time.model";
+import { switchMap } from "rxjs/operators";
+import toastr from "toastr";
 
 @Component({
   selector: 'app-time-form',
   templateUrl: './time-form.component.html',
   styleUrls: ['./time-form.component.css']
 })
-export class TimeFormComponent implements OnInit {
+export class TimeFormComponent implements OnInit,AfterContentChecked {
 
-  constructor() { }
+  currentAction: string;
+  TimeForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  time: Time = new Time();
 
+  constructor(
+    private timeService: TimeService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder      
+  ) { }
+
+  //metodo invocado na abertura da tela
   ngOnInit() {
+    //verifica a acao atual no form
+    this.setCurrentAction();
+    //cria o formulario
+    this.buildTimeForm();
+    //carrega o time caso seja edição
+    this.loadTime();
+  }
+
+  //metodo invocato apos a criacao dos componentes
+  ngAfterContentChecked(){
+    //atribui o titulo da pagina de forma dinamica
+    this.setPageTitle();
+  }
+
+  // METODOS PRIVADOS
+  private setCurrentAction(){
+    console.log(this.route.snapshot.url[0].path)
+    if(this.route.snapshot.url[0].path == "novo"){
+      this.currentAction = "novo";
+    }else{
+      this.currentAction = "editar";
+    }
+  }
+
+  private buildTimeForm(){
+    this.TimeForm = this.formBuilder.group({
+      id:[null],
+      nome: [null,Validators.required,Validators.minLength(3)],
+      descricao: [null]
+    })
+  }
+
+  private loadTime(){
+    if(this.currentAction == "editar"){
+      this.route.paramMap.pipe(
+        switchMap(params => this.timeService.getById(+params.get("id")))
+      ).subscribe(
+        (time) => {
+          this.time = time;
+          this.TimeForm.patchValue(time); // faz o bind de time para o formulario.
+        },
+        (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
+      )
+    }
+  }
+
+  private setPageTitle(){
+    if(this.currentAction == "novo"){
+      this.pageTitle = "Cadastro de Novo Time";
+    }else{
+      const nomeTime = this.time.nome || ""
+      this.pageTitle = "Editando time:" + nomeTime;
+    }
   }
 
 }
--- src/app/pages/times/times.module.ts
+++ src/app/pages/times/times.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { TimesRoutingModule } from './times-routing.module';
 import { TimeListComponent } from './time-list/time-list.component';
 import { TimeFormComponent } from './time-form/time-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TimeListComponent, TimeFormComponent],
   imports: [
     CommonModule,
-    TimesRoutingModule
+    TimesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class TimesModule { }
