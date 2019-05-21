3dff94fa2f6fb01bd3ce9af2f35f9bbab34ec4c5
Vasconcelos <j.a.vasconcelos@accenture.com>
2019-05-21 20:44:24 -0300

    criando o componente breadcrump

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -10,12 +11,14 @@ import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.compon
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule// necessario para funcionar o componente breadCrumb
   ],
   exports:[
     //shared modules
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,//exportacao
     BreadCrumbComponent,//exportacao do componente bread crumb para ser utlizado nas paginas
   ]
 })
