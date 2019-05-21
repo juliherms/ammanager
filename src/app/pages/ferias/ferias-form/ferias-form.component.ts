a9702f73f16c65f8d40506c4551494c708d77a3a
Vasconcelos <j.a.vasconcelos@accenture.com>
2019-05-21 13:51:08 -0300

    refatoracao do componente formulario de ferias

--- src/app/pages/ferias/ferias-form/ferias-form.component.ts
+++ src/app/pages/ferias/ferias-form/ferias-form.component.ts
@@ -2,27 +2,19 @@ import { TimeService } from './../../times/shared/time.service';
 import { Time } from './../../times/shared/time.model';
 import { Ferias } from './../shared/ferias.model';
 import { FeriasService } from './../shared/ferias.service';
-import { Component, OnInit, AfterContentChecked } from '@angular/core';
-import { FormBuilder,FormControl,FormGroup,Validators } from "@angular/forms";
-import { ActivatedRoute,Router } from "@angular/router";
-import { switchMap } from "rxjs/operators";
-import toastr from "toastr";
+import { Component,Injector,OnInit } from '@angular/core';
+import { Validators } from "@angular/forms";
+import { BaseResourceFormComponent } from "../../../shared/components/base-resource-form/base-resource-form.component";
 
-
+//Representa um formulario de ferias
 @Component({
   selector: 'app-ferias-form',
   templateUrl: './ferias-form.component.html',
   styleUrls: ['./ferias-form.component.css']
 })
-export class FeriasFormComponent implements OnInit,AfterContentChecked {
+export class FeriasFormComponent extends BaseResourceFormComponent<Ferias> implements OnInit {
 
-  currentAction: string;
-  feriasForm: FormGroup;
-  pageTitle: string;
-  serverErrorMessages: string[] = null;
-  submittingForm: boolean = false;
-  ferias: Ferias = new Ferias();
-  times: Array<Time>;//necessairo para fazer o select
+  times: Array<Time>;//necessario para fazer o select
 
   ptBR = {
     firsDayOfWeek: 0,
@@ -39,47 +31,18 @@ export class FeriasFormComponent implements OnInit,AfterContentChecked {
   }
 
   constructor(
-    private feriasService: FeriasService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private timeService: TimeService      
-  ) { }
+    protected feriasService: FeriasService,
+    protected timeService: TimeService,
+    protected injector: Injector
+  ) {
+    super(injector,new Ferias(),feriasService,Ferias.fromJson);
+   }
 
   //metodo invocado na abertura da tela
   ngOnInit() {
-    //verifica a acao atual no form
-    this.setCurrentAction();
-    //cria o formulario
-    this.buildFeriasForm();
-    //carrega o time caso seja edição
-    this.loadFerias();
     //carrega os times disponiveis
     this.loadTimes();
-  }
-
-  //metodo invocato apos a criacao dos componentes
-  ngAfterContentChecked(){
-    //atribui o titulo da pagina de forma dinamica
-    this.setPageTitle();
-  }
-
-  submitForm(){
-    this.submittingForm = true;
-    if(this.currentAction == "novo"){
-      this.createFerias();
-    }else{
-      this.updateFerias();
-    }
-  }
-
-  // METODOS PRIVADOS
-  private setCurrentAction(){
-    if(this.route.snapshot.url[0].path == "novo"){
-      this.currentAction = "novo";
-    }else{
-      this.currentAction = "editar";
-    }
+    super.ngOnInit();
   }
 
   private loadTimes(){
@@ -88,8 +51,9 @@ export class FeriasFormComponent implements OnInit,AfterContentChecked {
     );
   }
 
-  private buildFeriasForm(){
-    this.feriasForm = this.formBuilder.group({
+  protected buildResourceForm(){
+
+    this.resourceForm = this.formBuilder.group({
       id:[null],
       colaborador: [null, [Validators.required, Validators.minLength(3)]],
       comentario: [null],
@@ -100,65 +64,12 @@ export class FeriasFormComponent implements OnInit,AfterContentChecked {
     })
   }
 
-  private loadFerias(){
-    if(this.currentAction == "editar"){
-      this.route.paramMap.pipe(
-        switchMap(params => this.feriasService.getById(+params.get("id")))
-      ).subscribe(
-        (ferias) => {
-          this.ferias = ferias;
-          this.feriasForm.patchValue(ferias); // faz o bind de time para o formulario.
-        },
-        (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
-      )
-    }
+  protected creationPageTitle(): string{
+    return "Cadastro de Novas Férias";
   }
 
-  private setPageTitle(){
-    if(this.currentAction == "novo"){
-      this.pageTitle = "Cadastro de Novas férias";
-    }else{
-      const nomeFerias = this.ferias.colaborador || ""
-      this.pageTitle = "Editando as férias de: " + nomeFerias;
-    }
-  }
-
-  private createFerias(){
-    const ferias: Ferias =  Ferias.fromJson(this.feriasForm.value);
-    this.feriasService.create(ferias)
-    .subscribe(
-      time => this.actionsForSuccess(time),
-      error => this.actionsForError(error)
-    )
-  }
-
-  private updateFerias(){
-    const ferias: Ferias =  Ferias.fromJson(this.feriasForm.value);
-    this.feriasService.update(ferias)
-    .subscribe(
-      ferias => this.actionsForSuccess(ferias),
-      error => this.actionsForError(error)
-    )
-  }
-
-  private actionsForSuccess(ferias: Ferias){
-    //exibe uma mensagem na tela
-    toastr.success("Solicitação processada com sucesso!");
-    //redireciona para a pagina de edicao
-    this.router.navigateByUrl("ferias",{skipLocationChange:true}).then(
-      () => this.router.navigate(["ferias",ferias.id,"editar"])
-    )
-  }
-
-  private actionsForError(error){
-
-    toastr.error("Ocorreu um erro ao processar a sua solicitação!");
-
-    this.submittingForm = false;
-
-    if(error.status === 422)
-      this.serverErrorMessages = JSON.parse(error._body).errors;
-    else
-      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."]
+  protected editionPageTitle(): string {
+    const nomeFerias = this.resource.colaborador || "";
+    return "Editando Férias: " + nomeFerias;
   }
 }
