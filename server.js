60bbf1f22dec8ca82da017ed73e52691f8d0a9e7
Vasconcelos <j.a.vasconcelos@accenture.com>
2019-06-30 10:13:47 -0300

    atualizacao para deploy no heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/gestao-facil'));
+app.use(express.static(__dirname + '/dist/gestaoFacil'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/gestao-facil/index.html'));
+res.sendFile(path.join(__dirname+'/dist/gestaoFacil/index.html'));
 });
 
 
